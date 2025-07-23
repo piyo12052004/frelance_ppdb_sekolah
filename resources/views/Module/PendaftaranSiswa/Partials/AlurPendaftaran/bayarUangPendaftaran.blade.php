f6a4bee2763ba3a22fac8dff668946e7f4d109c8
nadialuthfia10 <luthfiaagp@gmail.com>
2025-07-23 21:33:01 +0700

    stashupi

--- resources/views/Module/PendaftaranSiswa/Partials/AlurPendaftaran/bayarUangPendaftaran.blade.php
+++ resources/views/Module/PendaftaranSiswa/Partials/AlurPendaftaran/bayarUangPendaftaran.blade.php
@@ -1,102 +1,16 @@
-    <div class="">bayar uang pendaftaran Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione
-        laboriosam
-        corrupti fugit odit distinctio nisi deserunt adipisci ab at debitis facere consectetur quia cum, expedita
-        placeat
-        voluptas vel laudantium voluptates quaerat cumque magni! Necessitatibus ducimus ratione dolor nam exercitationem
-        odit inventore, veritatis omnis nemo quasi harum! Porro quibusdam assumenda, minus dolor natus dignissimos
-        mollitia
-        ab laboriosam optio unde quasi facere error at, voluptatum, dolorum officiis neque perspiciatis ipsam maiores
-        esse
-        sunt fugiat voluptas. Quod aliquid architecto quibusdam iure, ullam totam! Accusamus impedit voluptatibus culpa,
-        eveniet voluptas tempore ut ad, optio ab dolor eligendi totam assumenda. Recusandae perferendis debitis quae
-        voluptas quis eum. Sunt aspernatur error hic provident assumenda aliquid nisi commodi enim ipsam nam odit,
-        exercitationem ea a consequatur culpa et, non voluptatum dicta explicabo aperiam ab debitis earum! Iste quia vel
-        quos expedita? Distinctio ipsum iste vitae ut non quod possimus, saepe eius recusandae veritatis ducimus minima
-        illum consectetur! Suscipit eveniet quia quo voluptates dolorum alias iusto, eum impedit porro! Illum labore
-        sint
-        explicabo nesciunt? Animi cumque impedit provident voluptatum qui suscipit facere nihil veritatis excepturi
-        mollitia
-        at consequuntur corrupti dolor nam iusto sunt, voluptatem atque, aliquid ea quae tempore, odit repudiandae
-        incidunt.
-        Reprehenderit nemo magnam iste quia. Sapiente, harum nemo. Deserunt iste necessitatibus quibusdam explicabo
-        sequi
-        reiciendis rerum, cupiditate temporibus laborum, similique adipisci voluptas assumenda, porro incidunt saepe eos
-        repudiandae? Repellendus esse nam, quis quasi cupiditate iure quas mollitia minima exercitationem doloribus
-        temporibus corporis laborum! Atque culpa pariatur quisquam veritatis et odit dolore voluptatem fugiat temporibus
-        repellat distinctio, ipsam doloribus asperiores quo voluptate repellendus omnis odio enim architecto at
-        laboriosam
-        facilis delectus ipsa? In sed voluptate nesciunt voluptatum natus libero aut rem veritatis, quisquam,
-        perferendis
-        eum sequi facere modi delectus dolorem sunt architecto esse. Ab voluptate possimus dolores fugiat rem iste quam
-        deserunt eveniet consequuntur. Animi, sunt. Neque iste nisi fuga repellat rem ullam suscipit optio odio vero,
-        explicabo, corrupti asperiores nam laudantium unde iure voluptatum iusto tempore inventore harum. Esse quidem
-        laudantium odio. Ipsam aut voluptas dolorem recusandae! Doloribus libero aut, earum possimus, veritatis, iure
-        quasi
-        quos pariatur molestias culpa maxime at necessitatibus ad odit praesentium adipisci quam dolorum nisi et.
-        Laudantium
-        amet saepe labore iusto culpa veritatis molestiae aliquam assumenda praesentium fugiat asperiores perferendis
-        officiis debitis vel, optio earum qui illo! Atque aspernatur corporis provident nesciunt tenetur, sequi quasi
-        eligendi, itaque ipsam distinctio facilis, repudiandae suscipit? Omnis tempore nulla recusandae magni ipsam eum
-        deleniti possimus, quod ducimus, aut aliquam amet et molestiae sunt quae, a quos saepe totam quam sit sequi ab
-        obcaecati itaque dolore. Exercitationem vel error voluptas illo asperiores molestias inventore esse praesentium?
-        Velit magni incidunt porro. Laboriosam ullam sequi nihil voluptatum quas quibusdam itaque minus eum? Ipsum non
-        quo
-        suscipit pariatur reprehenderit aut veniam quos dolores, vel blanditiis, hic iure ut fuga consequuntur nam neque
-        est
-        laudantium autem illum nulla repellat eveniet? Assumenda similique perspiciatis delectus, alias doloremque sequi
-        cupiditate in ab quidem fugiat sapiente unde id provident. Nihil eius dolorem iusto blanditiis, voluptate ab
-        magni,
-        voluptates accusantium facilis esse sunt possimus illum mollitia? Numquam nam rem repellendus quae eius at
-        deleniti
-        molestiae ducimus, impedit dignissimos totam vitae fugiat vero laudantium laboriosam quidem a ullam, modi unde
-        optio, dolor architecto nobis autem. Nihil sapiente autem veniam at consectetur laborum molestiae eius
-        perspiciatis
-        sunt ut optio iste, corrupti atque, impedit fuga eos eum dolor saepe repellat quaerat quos architecto. Sunt
-        illo,
-        aut corrupti ab voluptatibus nisi laboriosam a vitae culpa neque quos quis, distinctio necessitatibus optio in
-        itaque et dolore. Quas rem beatae velit, nihil debitis vitae ut dolores temporibus praesentium dolor, qui itaque
-        veritatis voluptas eaque pariatur totam voluptatum ea? Praesentium asperiores expedita voluptates dolore sit
-        quas,
-        eaque repellat unde velit modi consequuntur quod totam laudantium, temporibus, repellendus repudiandae nisi non?
-        Beatae deserunt labore consectetur, autem officia assumenda facilis, cupiditate tenetur eligendi enim porro
-        doloribus soluta ut expedita molestiae. Tempora quaerat quas officia porro nulla ut quo exercitationem amet
-        illo,
-        minus placeat repellendus reprehenderit vitae ducimus ratione mollitia, nisi aut maiores laborum sapiente magni
-        obcaecati non. Laboriosam sit perspiciatis odio voluptate accusantium necessitatibus at explicabo qui voluptatum
-        quia voluptates repudiandae, distinctio, soluta ipsam deserunt mollitia sequi optio magnam cupiditate incidunt
-        cum
-        possimus corporis. Eum reprehenderit totam aliquam sit dolorem dicta cumque hic cum qui molestiae. Blanditiis
-        soluta
-        necessitatibus ducimus corporis officiis delectus, sint eius esse ipsam corrupti, ex veniam inventore
-        voluptatibus
-        recusandae. Ad consequuntur quaerat voluptate, commodi ipsum quia voluptatum dolore nostrum? Saepe, minus.
-        Quidem
-        tempora nulla reiciendis odit atque vel laudantium! Natus, ipsum! Fugit, velit vitae dolore dolorum, ad officia,
-        animi deleniti delectus alias unde corrupti ratione quidem doloremque quaerat quam! Ad delectus quod, nesciunt
-        ut
-        eius nulla pariatur obcaecati iure commodi. Dolore accusantium iusto maxime perferendis rem aspernatur!
-        Molestiae
-        rem neque dolores blanditiis esse iusto quas excepturi qui sequi nihil animi eaque corporis dolorem eos corrupti
-        consectetur, nostrum nobis sint, libero quod praesentium voluptatum. Fuga, iste nihil ipsa sunt ex eius
-        veritatis
-        officia nulla facilis sint itaque recusandae fugit harum omnis labore similique. Voluptates nihil repellendus
-        sunt!
-        Hic saepe voluptate magni delectus a cumque adipisci quod similique! Tenetur praesentium quis officiis natus
-        laborum
-        quisquam eum magni debitis libero atque officia delectus nam doloribus vitae quod nihil fuga rem, recusandae
-        voluptas enim, minus adipisci, vero aliquid nemo! Voluptates quae impedit, quia culpa consequuntur doloremque
-        nam
-        similique sed provident recusandae. Ratione, dolorem cum aliquid nobis perspiciatis ducimus veniam, nesciunt non
-        totam adipisci provident corporis ab ex ad error voluptas qui unde, ullam impedit recusandae alias vitae in.
-        Esse,
-        dicta voluptas! Dolorum quae voluptatem nihil qui quos eos in, deleniti numquam? Optio officiis repellendus
-        animi
-        laborum, ex dolore iusto ab quia ducimus praesentium facere architecto, soluta quisquam cumque sunt non numquam
-        aliquam unde et mollitia? Facere facilis rerum reiciendis, voluptate quaerat id vitae debitis officiis delectus
-        in
-        nostrum exercitationem, veniam mollitia eveniet iusto quibusdam error nemo quia? Sequi amet, consectetur
-        consequatur
-        voluptatum, facere nostrum fugiat odio aperiam laudantium repellat ipsum vel beatae minima quaerat soluta modi
-        mollitia, illum quibusdam. Tempora, molestiae, praesentium laudantium facere eveniet, provident ipsam eligendi
-        aspernatur modi ratione sunt neque sit?
+<div class="">Adapun tata cara pembayaran via midtrans sebagai berikut:
+        <ol class="list-decimal ml-6 space-y-1 text-gray-700">
+            <li>Klik menu <strong>Pembayaran</strong> pada tampilan website.</li>
+            <li>klik menu <strong>Bayar Sekarang</strong>.</li>
+            <li>Pilih metode pembayaran.</li>
+            <li>Ikuti instruksi pembayaran sesuai metode yang dipilih.</li>
+            <li>Segarkan/Refresh tampilan website.</li>
+            <li>Status pembayaran akan otomatis berubah jika pembayaran sukses.</li>
+            <li>Peserta dapat melanjutkan isi formulir dan melengkapi dokumen yang diperlukan.</li>
+        </ol>
+
+    <div class="border-t pt-4 mt-6 text-gray-600 text-sm">
+        <p><strong>Catatan:</strong> Pengumuman penerimaan siswa dikirim via email.</p>
     </div>
+</div>
+</div>
--- resources/views/Module/PendaftaranSiswa/Partials/AlurPendaftaran/registrasi.blade.php
+++ resources/views/Module/PendaftaranSiswa/Partials/AlurPendaftaran/registrasi.blade.php
@@ -1,12 +1,27 @@
-<div class="">Adapun tata cara registrasi akun dan pendaftaran sekolah bagi calon siswa sebagai berikut :
-    <ol style="list-style: decimal; margin-left: 20px;">
-        <li>Pilih Daftar Siswa</li>
-        <li>Pilih Registrasi</li>
-        <li>Mengisi Formulir Online</li>
-        <li>Menunggu Verifikasi</li>
-        <li>Pilih Login</li>
-        <li>Ketik Username dan Password (saat melakukan registrasi)</li>
-        <li>Bayar Biaya Formulir</li>
-        <li>
-  </ol>
+<div class="">Adapun syarat pendaftaran sebagai berikut:
+    <div class="mb-8">
+        <div class="text-l font-semibold mb-2">Ketentuan Pendaftar</div>
+        <ol class="list-decimal ml-6 space-y-1 text-gray-700">
+            <li>Lulusan SD/MI sederajat</li>
+            <li>Usia maksimal saat mendaftar 15 Tahun</li>
+            <li>Sehat jasmani dan rohani.</li>
+            <li>Mengisi Formulir Pendaftaran</li>
+            <li>Bersedia mematuhi peraturan dan tata tertib sekolah.</li>
+        </ol>
+    </div>
+
+    <div class="mb-8">
+        <div class="text-l font-semibold mb-2">Dokumen yang Harus Disiapkan</div>
+        <ul class="list-disc ml-6 text-gray-700 space-y-1">
+            <li>Akta Kelahiran</li>
+            <li>Kartu Keluarga (KK)</li>
+            <li>Ijazah</li>
+            <li>Pas Foto Berwarna ukuran 3x4 (background merah)</li>
+        </ul>
+        <p class="text-gray-700 mt-3">Format file: <strong>PDF/JPG</strong>, maksimal <strong>2 MB</strong> per file.</p>
+    </div>
+    <div class="border-t pt-4 mt-6 text-gray-600 text-sm">
+        <p><strong>Catatan:</strong> Seluruh dokumen diunggah saat proses pengisian formulir pendaftaran online. Data yang tidak lengkap atau tidak sesuai ketentuan dapat menyebabkan proses pendaftaran ditolak.</p>
+    </div>
+</div>
 </div>
